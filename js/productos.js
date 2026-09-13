4381d2891a9ec7d7f067c6652a5c7dd39084c78c
polirritmico <ebray187@gmail.com>
2026-09-13 20:08:43 -0300

    feat(mantenedor): add delete product logic & update fake rows

--- js/productos.js
+++ js/productos.js
@@ -5,3 +5,29 @@ currentDate = new Intl.DateTimeFormat("es-CL", {
 }).format(new Date());
 
 document.getElementById("id-date").textContent = currentDate;
+
+const modalEliminarProducto = document.getElementById(
+  "modal-eliminar-producto",
+);
+const btnConfirmarEliminar = document.getElementById(
+  "modal-eliminar-producto-btn-confirmar",
+);
+
+if (modalEliminarProducto && btnConfirmarEliminar) {
+  modalEliminarProducto.addEventListener("show.bs.modal", (ev) => {
+    const botonTrigger = ev.relatedTarget;
+    const idProducto = botonTrigger.getAttribute("data-jk-id");
+
+    btnConfirmarEliminar.setAttribute("data-target-id", idProducto);
+  });
+
+  btnConfirmarEliminar.addEventListener("click", (ev) => {
+    const idProducto = ev.target.getAttribute("data-target-id");
+    console.log(`Eliminando producto ID: ${idProducto}`);
+
+    const fila = document.getElementById(idProducto);
+    if (fila) {
+      fila.remove();
+    }
+  });
+}
